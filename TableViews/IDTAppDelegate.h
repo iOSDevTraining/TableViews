baa9dec3d848cd1c96fd0adcf76a37ff8a830f70
Josh Brown <josh@roadfiresoftware.com>
2013-11-07 16:17:18 -0500

    created project

---
+++ TableViews/IDTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IDTAppDelegate.h
+//  TableViews
+//
+//  Created by Josh Brown on 11/7/13.
+//  Copyright (c) 2013 iOS Dev Training. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableViews
+//
+//  Created by Josh Brown on 11/7/13.
+//  Copyright (c) 2013 iOS Dev Training. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IDTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IDTAppDelegate class]));
+    }
+}
